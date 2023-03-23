681cbdeed2abcf6c801934135634285926b591d8
andrewGTR <andrewgergeskadis@gmail.com>
2023-03-23 15:19:43 -0700

    ceco

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-* main - causes an infinite loop
-* Return: 0
+ * main - causes an infinite loop
+ * Return: 0
 */
 
 int main(void)
@@ -12,6 +12,7 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
+
 	/*while (i < 10)*/
 	/*{*/
 	/*putchar(i);*/
